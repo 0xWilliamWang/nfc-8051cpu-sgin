ff930c302f749a5932f939a4ef300665bdea11af
wangkaixuan <2654189525@qq.com>
2021-01-06 16:54:40 +0800

    show

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ecc.h"
+//#include "ecc.h"
 
 void UART_Init()
 {
@@ -25,11 +25,14 @@ void UART_Send_Data(unsigned char byte)
     TI = 0;
 }
 
-void show(char *str, int str_len)
+void show(unsigned char *hex, int hex_size)
 {
     int index = 0;
-    for (; index < str_len; index++)
-        UART_Send_Data(str[index]);
+    unsigned char ch = 0;
+    for (; index <= hex_size; index++){
+        ch = hex[index] >= 10 ? 0x37 + hex[index] : 0x30 + hex[index];
+        UART_Send_Data(ch);
+    }
 }
 
 void printHex(unsigned char *hex, int hex_len)
@@ -44,10 +47,16 @@ void printHex(unsigned char *hex, int hex_len)
 
 void main()
 {
-    char name[] = "wangkaixuan";
+    unsigned char hexs[3];
+    hexs[0] = 9;
+    hexs[1] = 2;
+    hexs[2] = 4;
     UART_Init();
     UART_Send_Data('w');
-    show(name, strlen(name));
+    UART_Send_Data('0' + hexs[0]);
+    UART_Send_Data('0' + hexs[1]);
+    UART_Send_Data('0' + hexs[2]);
+    // show(hexs,3);
     UART_Send_Data('k');
     UART_Send_Data('x');
     while (1);
