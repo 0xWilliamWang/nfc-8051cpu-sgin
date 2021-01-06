8c47c19d3e8301c9f0d08e50acc2236960b2ec6b
wangkaixuan <2654189525@qq.com>
2021-01-06 22:07:37 +0800

    normal work at sim

--- main.c
+++ main.c
@@ -27,58 +27,43 @@ void UART_Send_Data(unsigned char byte)
 
 int ecsda_test(void)
 {
+    int ret = 0;
+    uint8_t signatureR[NUM_ECC_DIGITS];
+    uint8_t signatureS[NUM_ECC_DIGITS];
+    uint8_t publicX[NUM_ECC_DIGITS];
+    uint8_t publicY[NUM_ECC_DIGITS];
+
+
     EccPoint l_public;
     uint8_t l_private[NUM_ECC_DIGITS];
     uint8_t r[NUM_ECC_DIGITS];
     uint8_t s[NUM_ECC_DIGITS];
     uint8_t result[NUM_ECC_DIGITS] = {0};
-    uint8_t l_random[NUM_ECC_DIGITS] = {0x2c, 0xf2, 0x4d, 0xba, 0x5f, 0xb0, 0xa3, 0x0e, 0x26, 0xe8, 0x3b, 0x2a, 0xc5, 0xb9, 0xe2, 0x9e, 0x1b, 0x16, 0x1e, 0x5c, 0x1f, 0xa7, 0x42, 0x5e, 0x73, 0x04, 0x33, 0x62, 0x93, 0x8b, 0x98, 0x24};
+    uint8_t l_random[NUM_ECC_DIGITS] = {0x09, 0xa2, 0x34, 0x01, 0x48, 0xfc, 0x54, 0x7b, 0xf1, 0xf6, 0x44, 0x93, 0x83, 0xf4, 0x88, 0x3b, 0xd2, 0xf6, 0x46, 0x39, 0xa4, 0x9b, 0x39, 0x8a, 0x1b, 0x48, 0x46, 0x8d, 0xa3, 0x82, 0x0d, 0x0a};
 
     // hello
     uint8_t l_hash[NUM_ECC_DIGITS] = {0};
     uint8_t tmp_hash[NUM_ECC_DIGITS] = {0x2c, 0xf2, 0x4d, 0xba, 0x5f, 0xb0, 0xa3, 0x0e, 0x26, 0xe8, 0x3b, 0x2a, 0xc5, 0xb9, 0xe2, 0x9e, 0x1b, 0x16, 0x1e, 0x5c, 0x1f, 0xa7, 0x42, 0x5e, 0x73, 0x04, 0x33, 0x62, 0x93, 0x8b, 0x98, 0x24};
     ecc_bytes2native(l_hash, tmp_hash);
 
-
-
     ecc_make_key(&l_public, l_private, l_random);
 
-    if (!ecdsa_sign(r, s, l_private, l_random, l_hash))
-    {
-        UART_Send_Data('x');
-        UART_Send_Data('1');
-    }
+    ret = ecdsa_sign(r, s, l_private, l_random, l_hash);
+    ecc_bytes2native(signatureR, r);
+    ecc_bytes2native(signatureS, s);
+    ecc_bytes2native(publicX, l_public.x);
+    ecc_bytes2native(publicY, l_public.y);
+    // if (!ecc_valid_public_key(&l_public))
+    // {
+    //     UART_Send_Data('x');
+    //     UART_Send_Data('2');
+    // }
 
-    if (!ecc_valid_public_key(&l_public))
-    {
-        UART_Send_Data('x');
-        UART_Send_Data('2');
-    }
-
-    if (!ecdsa_verify(&l_public, l_hash, r, s))
-    {
-        UART_Send_Data('x');
-        UART_Send_Data('3');
-    }
-    // printf("{\n");
-    // ecc_native2bytes(result, l_private);
-    // printHex("\"private\"", result, NUM_ECC_DIGITS);
-
-    // printHex("\"hash\"", l_hash, NUM_ECC_DIGITS);
-
-    // ecc_native2bytes(result, r);
-    // printHex("\"r\"", result, NUM_ECC_DIGITS);
-
-    // ecc_native2bytes(result, s);
-    // printHex("\"s\"", result, NUM_ECC_DIGITS);
-
-    // ecc_native2bytes(result, l_public.x);
-    // printHex("\"publicX\"", result, NUM_ECC_DIGITS);
-
-    // ecc_native2bytes(result, l_public.y);
-    // printHex("\"publicY\"", result, NUM_ECC_DIGITS);
-    // printf("},\n");
-
+    // if (!ecdsa_verify(&l_public, l_hash, r, s))
+    // {
+    //     UART_Send_Data('x');
+    //     UART_Send_Data('3');
+    // }
     return 1;
 }
 
@@ -104,12 +89,7 @@ void printHex(unsigned char *hex, int hex_len)
 
 void main()
 {
-    char name[] = "wangkaixuan";
-    UART_Init();
-    UART_Send_Data('w');
-    show(name, strlen(name));
+    int i = 0;
     ecsda_test();
-    UART_Send_Data('k');
-    UART_Send_Data('x');
-    while (1);
+    i++;
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+save signatureR.txt X:0x000004,X:0x000023
+save signatureS.txt X:0x000024,X:0x000043
+save publicX.txt X:0x000044,X:0x000063
+save publicY.txt X:0x000064,X:0x000083
